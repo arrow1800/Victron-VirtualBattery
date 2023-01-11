a3061a6b50a6576f3a0dbc570b36af6f410046b9
ahuismaneiffel <44672786+ahuismaneiffel@users.noreply.github.com>
2023-01-11 23:10:37 +0100

    -

--- README.md
+++ README.md
@@ -1,20 +1,24 @@
 # Victron virtual battery
 
-this is a dbus service for the Victron venus OS. it is a virtual battery that doesn't do anything besides being available as a real battery in de venus OS. this is the only way to make sure that Venus OS and al its connected devices will honor the settings and information that this battery sends.
+this is a dbus service for the Victron venus OS. It is a virtual battery that doesn't do anything besides being available as a real battery in the venus OS. This is the only way to make sure that Venus OS and all its connected devices will honor the settings and information that this battery produces.
 
-The idea behind this is that when you have a virtual battery available in Venus OS that it can be configured as the default battery. after that you are able to write data to it from any device (multiplusses, one or more BMS'es, smartshunts, etc.). when the information is stored in the virtual battery Venus OS will take care of the rest (making sure all chargers/devices receive the correct information and behave correctly) 
+The idea behind this is that when you have a virtual battery available in Venus OS it can be configured as the default battery. after that you are able to write any combination of data to it from any device (multiplusses, one or more BMS'es, smartshunts, etc.). When the information is stored in the virtual battery Venus OS will take care of the rest (making sure all chargers/devices receive the correct information and behave accordingly) 
 
 You can write data to the virtual battery using node-red with the Venus OS Large image. 
 
+[screenshot of virtual battery in venus os](img/virtualbattery-screenshot.png)
+[screenshot of virtual battery in venus os](img/virtualbattery-screenshot1.png)
+
 ## Node-red
 
-node-red gives you the option to read/extract information from all kinds of devices available on the dbus or from anywhere else. that information can be transformed/aggregated in any form and can be used to execute several actions (such as writing the right information to the virtual battery). A few ideas:
+node-red gives you the option to read/extract information from all kinds of devices available on the dbus or from anywhere else. That information can be transformed/aggregated in any form and can be used to execute several actions (such as writing the right information to the virtual battery). A few ideas:
 
-- Use cell voltages read from a specific BMS that you are using
+- Use cell voltages read from one or more specific BMS'es that you are using
 - Use SOC readings from a victron smartshunt (higher accuracy)
-- Use Amp reading from the multiplus itself, a specific BMS or a smartshunt
+- Use Amps reading from the multiplus itself, a specific BMS or a smartshunt
+- Any of the above in a combined way
 
-This way you have full control over how and which information flows through the system. You are also able to create your own logic rules, such as; 
+This way you have full control over how and which information flows through the system. You are also able to create your own logic rules with node-red, such as; 
 
 - When one cell voltage is to high, lower max charge current
 - When one cell voltage is to low, lower discharge current
@@ -26,52 +30,23 @@ This way you have full control over how and which information flows through the 
 
 ## Example flow
 
-...
+see file exampleflow.json you can copy the contents to node-red and import it all at once.
+
+[screenshot of node-red flow](img/node-red-screenshot.png)
 
 ## How to - CerboGX
 
 Installation:
 - create /data/dbus-virtual-battery directory
-- copy the stuff into it
+- copy files to this folder
 - set chmod 744 for ./service/run and ./restart
-- add command ln -s /data/dbus-virtual-battery/service /service/dbus-virtual-battery into /data/rc.local
-
-Configuration:
-- make sure you have enabled modbus over TCP, this is needed for writing new data to the virtual battery
-
-## How to - Node-red
-
-- install this extra package from the node-red palette: ..........
+- add this line to the file /data/rc.local : ln -s /data/dbus-virtual-battery/service /service/dbus-virtual-battery 
 
 
-
-The service starts automatically after start/restart of the Venus OS. After changing of aggregatebatteries.py or
-settings.py restart it by executing:
-
-./restart - it kills the service which starts automatically again.
-
-For debugging (to see the error messages in the console) it could be reasonable to rename: ./service/run 
-and start by: python3 aggregatebatteries.py
+The service now starts automatically after a start/restart of the Venus OS
 
 ### Defaults
 
 All default values can be changed from within the node-red ui. 
 
 
-	
-The max. charge voltage is either set to (CHARGE_VOLTAGE * Nr. of cells) or is limited to the immediate battery voltage if the first cell reaches 
-the MAX_CELL_VOLTAGE. This avoids emergency disconnecting the battery by its BMS.    
-	
-The MaxChargeCurrent is reduced from MAX_CHARGE_CURRENT to MAX_CHARGE_CURRENT_ABOVE_CV1 when the first cell reaches CV1 and further
-reduced to MAX_CHARGE_CURRENT_ABOVE_CV2 when the first cell reaches CV2.
-
-The MaxDischargeCurrent is reduced from MAX_DISCHARGE_CURRENT to zero if the battery voltage falls down to (DISCHARGE_VOLTAGE * Nr. of cells)
-or the first cell falls down to MIN_CELL_VOLTAGE.
-
-The charge or discharge current is set to zero if at least one BMS is blocking charge or discharge respectively.
-
-Logging file:
-./service/aggregatebatteries.log	
-
-Known issue:
-- the last data from dbus-serialbattery driver remain on Dbus if the connection is interrupted. Therefore my software cannot recognize it as well.
--- virtualbattery.py
+++ virtualbattery.py
@@ -1,9 +1,3 @@
-"""
-Service to create a virtual battery within the venus os
-"""
-
-VERSION = '0.1'
-
 from gi.repository import GLib
 import logging
 import sys
@@ -12,9 +6,11 @@ import dbus
 # from settings import *
 from datetime import datetime as dt         # for UTC time stamps for logging
 import time as tt                           # for charge measurement
+import requests
+import json
 
 sys.path.append('/opt/victronenergy/dbus-systemcalc-py/ext/velib_python')
-from vedbus import VeDbusService, VeDbusItemImport
+from vedbus import VeDbusService #, VeDbusItemImport
 
 class DbusVirtualBatService(object):
     
@@ -24,7 +20,7 @@ class DbusVirtualBatService(object):
         
         # Create the mandatory objects
         self._dbusservice.add_mandatory_paths(processname = __file__, processversion = '0.0', connection = 'Virtual',
-			deviceinstance = 15, productid = 0, productname = 'VirtualBattery', firmwareversion = VERSION, 
+			deviceinstance = 15, productid = 0, productname = 'VirtualBattery', firmwareversion = 0.1, 
             hardwareversion = '0.0', connected = 1)
 
         # Create DC paths        
@@ -34,146 +30,105 @@ class DbusVirtualBatService(object):
         
         # Create capacity paths
         self._dbusservice.add_path('/Soc', None, writeable=True)
-        self._dbusservice.add_path('/Capacity', None, writeable=True, gettextcallback=lambda a, x: "{:.0f}Ah".format(x))
-        self._dbusservice.add_path('/InstalledCapacity', None, gettextcallback=lambda a, x: "{:.0f}Ah".format(x))
-        self._dbusservice.add_path('/ConsumedAmphours', None, gettextcallback=lambda a, x: "{:.0f}Ah".format(x))
+        # self._dbusservice.add_path('/Capacity', None, writeable=True, gettextcallback=lambda a, x: "{:.0f}Ah".format(x))
+        # self._dbusservice.add_path('/InstalledCapacity', None, gettextcallback=lambda a, x: "{:.0f}Ah".format(x))
+        # self._dbusservice.add_path('/ConsumedAmphours', None, gettextcallback=lambda a, x: "{:.0f}Ah".format(x))
         
         # Create temperature paths
-        self._dbusservice.add_path('/Dc/0/Temperature', None, writeable=True)       
+        # self._dbusservice.add_path('/Dc/0/Temperature', None, writeable=True)       
         self._dbusservice.add_path('/System/MinCellTemperature', None, writeable=True)
         self._dbusservice.add_path('/System/MaxCellTemperature', None, writeable=True)
-
-        #self._dbusservice.add_path('/System/MaxTemperatureCellId', None, writeable=True)       
-        #self._dbusservice.add_path('/System/MinTemperatureCellId', None, writeable=True)
+        # self._dbusservice.add_path('/System/MaxTemperatureCellId', None, writeable=True)       
+        # self._dbusservice.add_path('/System/MinTemperatureCellId', None, writeable=True)
         
         # Create extras paths
         self._dbusservice.add_path('/System/MinCellVoltage', None, writeable=True)
-        self._dbusservice.add_path('/System/MinVoltageCellId', None, writeable=True)
+        # self._dbusservice.add_path('/System/MinVoltageCellId', None, writeable=True)
         self._dbusservice.add_path('/System/MaxCellVoltage', None, writeable=True)
-        self._dbusservice.add_path('/System/MaxVoltageCellId', None, writeable=True)
-        self._dbusservice.add_path('/System/NrOfCellsPerBattery', None, writeable=True)
-        self._dbusservice.add_path('/System/NrOfModulesOnline', None, writeable=True)
-        self._dbusservice.add_path('/System/NrOfModulesOffline', None, writeable=True)
-        self._dbusservice.add_path('/System/NrOfModulesBlockingCharge', None, writeable=True)
-        self._dbusservice.add_path('/System/NrOfModulesBlockingDischarge', None, writeable=True)         
+        # self._dbusservice.add_path('/System/MaxVoltageCellId', None, writeable=True)
+        # self._dbusservice.add_path('/System/NrOfCellsPerBattery', None, writeable=True)
+        # self._dbusservice.add_path('/System/NrOfModulesOnline', None, writeable=True)
+        # self._dbusservice.add_path('/System/NrOfModulesOffline', None, writeable=True)
+        # self._dbusservice.add_path('/System/NrOfModulesBlockingCharge', None, writeable=True)
+        # self._dbusservice.add_path('/System/NrOfModulesBlockingDischarge', None, writeable=True)         
         
         # Create alarm paths
-        self._dbusservice.add_path('/Alarms/LowVoltage', None, writeable=True)
-        self._dbusservice.add_path('/Alarms/HighVoltage', None, writeable=True)
-        self._dbusservice.add_path('/Alarms/LowCellVoltage', None, writeable=True)
-        self._dbusservice.add_path('/Alarms/LowSoc', None, writeable=True)
-        self._dbusservice.add_path('/Alarms/HighChargeCurrent', None, writeable=True)
-        self._dbusservice.add_path('/Alarms/HighDischargeCurrent', None, writeable=True)
-        self._dbusservice.add_path('/Alarms/CellImbalance', None, writeable=True)
-        self._dbusservice.add_path('/Alarms/InternalFailure', None, writeable=True)
-        self._dbusservice.add_path('/Alarms/HighChargeTemperature', None, writeable=True)
-        self._dbusservice.add_path('/Alarms/LowChargeTemperature', None, writeable=True)
-        self._dbusservice.add_path('/Alarms/HighTemperature', None, writeable=True)
-        self._dbusservice.add_path('/Alarms/LowTemperature', None, writeable=True)
+        # self._dbusservice.add_path('/Alarms/LowVoltage', None, writeable=True)
+        # self._dbusservice.add_path('/Alarms/HighVoltage', None, writeable=True)
+        # self._dbusservice.add_path('/Alarms/LowCellVoltage', None, writeable=True)
+        # self._dbusservice.add_path('/Alarms/LowSoc', None, writeable=True)
+        # self._dbusservice.add_path('/Alarms/HighChargeCurrent', None, writeable=True)
+        # self._dbusservice.add_path('/Alarms/HighDischargeCurrent', None, writeable=True)
+        # self._dbusservice.add_path('/Alarms/CellImbalance', None, writeable=True)
+        # self._dbusservice.add_path('/Alarms/InternalFailure', None, writeable=True)
+        # self._dbusservice.add_path('/Alarms/HighChargeTemperature', None, writeable=True)
+        # self._dbusservice.add_path('/Alarms/LowChargeTemperature', None, writeable=True)
+        # self._dbusservice.add_path('/Alarms/HighTemperature', None, writeable=True)
+        # self._dbusservice.add_path('/Alarms/LowTemperature', None, writeable=True)
         #self._dbusservice.add_path('/Alarms/HighCellVoltage', None, writeable=True)
         
         # Create control paths
         self._dbusservice.add_path('/Info/MaxChargeCurrent', None, writeable=True, gettextcallback=lambda a, x: "{:.0f}A".format(x))
         self._dbusservice.add_path('/Info/MaxDischargeCurrent', None, writeable=True, gettextcallback=lambda a, x: "{:.0f}A".format(x))
         self._dbusservice.add_path('/Info/MaxChargeVoltage', None, writeable=True, gettextcallback=lambda a, x: "{:.1f}V".format(x))
+
+        GLib.timeout_add(1000, self._update)    
     
-    
-    #########################################    
-    # update Dbus
-    #########################################
     
     def _update(self):  
-        
-        # # DC
-        # Voltage = 0
-        # Current = 0
-        # Power = 0
-        
-        # # Capacity
-        # Soc = 0
-        # Capacity = 0
-        # InstalledCapacity = 0
-        # ConsumedAmphours = 0        
-        
-        # # Temperature
-        # Temperature = 0
-        # MaxCellTemperature = []    
-        # MinCellTemperature = []  
-        
-        # # Extras
-        # MaxCellVoltage = {}         # dictionary {'ID' : MaxCellVoltage, ... } for all physical batteries
-        # MinCellVoltage = {}         # dictionary {'ID' : MinCellVoltage, ... } for all physical batteries        
-        # NrOfCellsPerBattery = []    # list, NRofCells of all physical batteries (shall be the same)
-        # NrOfModulesOnline = 0
-        # NrOfModulesOffline = 0
-        # NrOfModulesBlockingCharge = 0
-        # NrOfModulesBlockingDischarge = 0
-        
-        # # Alarms
-        # LowVoltage_alarm = []       # lists to find maxima
-        # HighVoltage_alarm = []
-        # LowCellVoltage_alarm = []
-        # #HighCellVoltage_alarm = []
-        # LowSoc_alarm = []
-        # HighChargeCurrent_alarm = []
-        # HighDischargeCurrent_alarm = []
-        # CellImbalance_alarm = []
-        # InternalFailure_alarm = []
-        # HighChargeTemperature_alarm = []
-        # LowChargeTemperature_alarm = []
-        # HighTemperature_alarm = []
-        # LowTemperature_alarm = []
 
-        # with self._dbusservice as bus:
         
-        #     # send DC
-        #     # bus['/Dc/0/Voltage'] = round(Voltage, 1)
-        #     # bus['/Dc/0/Current'] = round(Current, 1)
-        #     # bus['/Dc/0/Power'] = round(Power, 0)
-        
-        #     # send capacity
-        #     bus['/Soc'] = Soc
-        #     bus['/Capacity'] = Capacity
-        #     bus['/InstalledCapacity'] = InstalledCapacity
-        #     bus['/ConsumedAmphours'] = ConsumedAmphours
-        
-        #     # send temperature
-        #     # bus['/Dc/0/Temperature'] = Temperature
-        #     # bus['/System/MaxCellTemperature'] = MaxCellTemp
-        #     # bus['/System/MinCellTemperature'] = MinCellTemp
-        
-        #     # send cell min/max voltage
-        #     # bus['/System/MaxCellVoltage'] = MaxCellVoltage
-        #     # bus['/System/MaxVoltageCellId'] = MaxVoltageCellId
-        #     # bus['/System/MinCellVoltage'] = MinCellVoltage
-        #     # bus['/System/MinVoltageCellId'] = MinVoltageCellId
-        
-        #     # send battery state
-        #     bus['/System/NrOfCellsPerBattery'] = NrOfCellsPerBattery
-        #     bus['/System/NrOfModulesOnline'] = NrOfModulesOnline
-        #     bus['/System/NrOfModulesOffline'] = NrOfModulesOffline
-        #     bus['/System/NrOfModulesBlockingCharge'] = NrOfModulesBlockingCharge
-        #     bus['/System/NrOfModulesBlockingDischarge'] = NrOfModulesBlockingDischarge
-        
-        #     # send alarms
-        #     bus['/Alarms/LowVoltage'] = LowVoltage_alarm
-        #     bus['/Alarms/HighVoltage'] = HighVoltage_alarm
-        #     bus['/Alarms/LowCellVoltage'] = LowCellVoltage_alarm
-        #     #bus['/Alarms/HighCellVoltage'] = HighCellVoltage_alarm   # not implemended in Venus
-        #     bus['/Alarms/LowSoc'] = LowSoc_alarm
-        #     bus['/Alarms/HighChargeCurrent'] = HighChargeCurrent_alarm
-        #     bus['/Alarms/HighDischargeCurrent'] = HighDischargeCurrent_alarm
-        #     bus['/Alarms/CellImbalance'] = CellImbalance_alarm
-        #     bus['/Alarms/InternalFailure'] = InternalFailure_alarm
-        #     bus['/Alarms/HighChargeTemperature'] = HighChargeTemperature_alarm
-        #     bus['/Alarms/LowChargeTemperature'] = LowChargeTemperature_alarm
-        #     bus['/Alarms/HighTemperature'] = HighChargeTemperature_alarm
-        #     bus['/Alarms/LowTemperature'] = LowChargeTemperature_alarm
-        
-        #     # send charge/discharge control
-        #     bus['/Info/MaxChargeCurrent'] = MaxChargeCurrent
-        #     bus['/Info/MaxDischargeCurrent'] = MaxDischargeCurrent
-        #     bus['/Info/MaxChargeVoltage'] = MaxChargeVoltage
+        try:
+            
+            r = requests.get('https://localhost:1881/virtualbattery', verify=False)
+
+            json = r.json()
+            
+            with self._dbusservice as bus:
+            
+                bus['/Dc/0/Voltage'] = round(json['Voltage'], 1)
+                bus['/Dc/0/Current'] = round(json['Current'], 1)
+                bus['/Dc/0/Power'] = round(json['Power'], 0)
+            
+                bus['/Soc'] = json['Soc']
+                # bus['/Capacity'] = Capacity
+                # bus['/InstalledCapacity'] = InstalledCapacity
+                # bus['/ConsumedAmphours'] = ConsumedAmphours
+            
+                # bus['/Dc/0/Temperature'] = Temperature
+                bus['/System/MaxCellTemperature'] = json['MaxCellTemperature']
+                bus['/System/MinCellTemperature'] = json['MinCellTemperature']
+            
+                bus['/System/MaxCellVoltage'] = json['MaxCellVoltage']
+                # bus['/System/MaxVoltageCellId'] = MaxVoltageCellId
+                bus['/System/MinCellVoltage'] = json['MinCellVoltage']
+                # bus['/System/MinVoltageCellId'] = MinVoltageCellId
+            
+                # bus['/System/NrOfCellsPerBattery'] = NrOfCellsPerBattery
+                # bus['/System/NrOfModulesOnline'] = NrOfModulesOnline
+                # bus['/System/NrOfModulesOffline'] = NrOfModulesOffline
+                # bus['/System/NrOfModulesBlockingCharge'] = NrOfModulesBlockingCharge
+                # bus['/System/NrOfModulesBlockingDischarge'] = NrOfModulesBlockingDischarge
+            
+                # bus['/Alarms/LowVoltage'] = LowVoltage_alarm
+                # bus['/Alarms/HighVoltage'] = HighVoltage_alarm
+                # bus['/Alarms/LowCellVoltage'] = LowCellVoltage_alarm
+                # bus['/Alarms/LowSoc'] = LowSoc_alarm
+                # bus['/Alarms/HighChargeCurrent'] = HighChargeCurrent_alarm
+                # bus['/Alarms/HighDischargeCurrent'] = HighDischargeCurrent_alarm
+                # bus['/Alarms/CellImbalance'] = CellImbalance_alarm
+                # bus['/Alarms/InternalFailure'] = InternalFailure_alarm
+                # bus['/Alarms/HighChargeTemperature'] = HighChargeTemperature_alarm
+                # bus['/Alarms/LowChargeTemperature'] = LowChargeTemperature_alarm
+                # bus['/Alarms/HighTemperature'] = HighChargeTemperature_alarm
+                # bus['/Alarms/LowTemperature'] = LowChargeTemperature_alarm
+            
+                bus['/Info/MaxChargeCurrent'] = json['MaxChargeCurrent']
+                bus['/Info/MaxDischargeCurrent'] = json['MaxDischargeCurrent']
+                bus['/Info/MaxChargeVoltage'] = json['MaxChargeVoltage']
+        except:
+            logging.info(f'{dt.now()} error occurred during update, retrying..')
+            GLib.timeout_add(5000, self._update) 
 
         return True
     
@@ -184,7 +139,9 @@ def main():
     # Have a mainloop, so we can send/receive asynchronous calls to and from dbus
     DBusGMainLoop(set_as_default=True)
     DbusVirtualBatService()
-    logging.info('%s: Connected to dbus, and switching over to GLib.MainLoop() (= event based)' % dt.now())
+
+    logging.info(f'{dt.now()} Connected to dbus')
+
     mainloop = GLib.MainLoop()
     mainloop.run()
 
