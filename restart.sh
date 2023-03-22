07fba85f93bdc728c16556a5198eafa72bcd514d
arrow1800 <arrow1800@hotmail.com>
2023-03-22 11:47:38 +0100

    added comments

--- restart.sh
+++ restart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+#only kill the process, venusos will start automatically
 kill $(pgrep -f "python $SCRIPT_DIR/virtualbattery.py")
