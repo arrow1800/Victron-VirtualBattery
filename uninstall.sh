ba9e3353bae17108e521645eab8262dd2481e10b
arrow1800 <arrow1800@hotmail.com>
2023-03-23 17:15:12 +0100

    changed process name

--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SERVICE_NAME=$(basename $SCRIPT_DIR)
 
 rm /service/$SERVICE_NAME
-kill $(pgrep -f 'supervise virtualbattery')
+kill $(pgrep -f 'supervise dbus-virtual-battery')
 chmod a-x $SCRIPT_DIR/service/run
 ./"$SCRIPT_DIR/restart.sh"
