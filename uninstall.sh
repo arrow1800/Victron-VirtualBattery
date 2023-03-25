ff13e8fbe7bc7742978cbbeb80bc6b7c083ebd2c
arrow1800 <arrow1800@hotmail.com>
2023-03-25 18:15:13 +0100

    added line so driver isnt reinstalled on reboot

--- uninstall.sh
+++ uninstall.sh
@@ -5,4 +5,7 @@ SERVICE_NAME=$(basename $SCRIPT_DIR)
 rm /service/$SERVICE_NAME
 kill $(pgrep -f 'supervise dbus-virtual-battery')
 chmod a-x $SCRIPT_DIR/service/run
+
+sed -i '/dbus-virtual-battery/d' /data/rc.local
+
 ./"$SCRIPT_DIR/restart.sh"
